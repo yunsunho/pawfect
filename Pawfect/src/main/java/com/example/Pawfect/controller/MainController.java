72512e4888784e340198db032e0ee2546fe7415c
yunsunho <yunsh0613@skuniv.ac.kr>
2025-04-04 15:31:29 +0900

    main.html

---
+++ Pawfect/src/main/java/com/example/Pawfect/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.Pawfect.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "main"; // main.jsp 또는 main.html이 렌더링됨
+    }
+}
