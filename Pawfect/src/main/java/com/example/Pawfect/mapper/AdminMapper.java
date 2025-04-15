34e87a6f73d82661fbc4022c09341a0451439b14
Seunghyun Nam <shyunam@gmail.com>
2025-04-15 10:21:00 +0900

    feat: Add admin service logic

--- Pawfect/src/main/java/com/example/Pawfect/mapper/AdminMapper.java
+++ Pawfect/src/main/java/com/example/Pawfect/mapper/AdminMapper.java
@@ -1,12 +1,23 @@
 package com.example.Pawfect.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.Pawfect.dto.CommentDto;
+import com.example.Pawfect.dto.InquiryDto;
+import com.example.Pawfect.dto.PostDto;
+import com.example.Pawfect.dto.ReviewDto;
 import com.example.Pawfect.dto.UserDto;
 
 @Mapper
 public interface AdminMapper {
-	public List<UserDto> getAllUsers();
+	public List<UserDto> getAllUsers(Map<String, Object> map);
+	public List<PostDto> getAllPosts(Map<String, Object> map);
+	public List<CommentDto> getAllComments(Map<String, Object> map);
+	public List<ReviewDto> getAllReviews(Map<String, Object> map);
+	public List<InquiryDto> getAllInquiries(Map<String, Object> map);
+	public List<InquiryDto> getHandledInquiries(Map<String, Object> map);
+	public List<InquiryDto> getUnhandledInquiries(Map<String, Object> map);
 }
--- Pawfect/src/main/java/com/example/Pawfect/service/AdminService.java
+++ Pawfect/src/main/java/com/example/Pawfect/service/AdminService.java
@@ -1,10 +1,15 @@
 package com.example.Pawfect.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.Pawfect.dto.CommentDto;
+import com.example.Pawfect.dto.InquiryDto;
+import com.example.Pawfect.dto.PostDto;
+import com.example.Pawfect.dto.ReviewDto;
 import com.example.Pawfect.dto.UserDto;
 import com.example.Pawfect.mapper.AdminMapper;
 
@@ -13,7 +18,31 @@ public class AdminService {
 	@Autowired
 	private AdminMapper adminMapper;
 	
-	public List<UserDto> getAllUsers() {
-		return adminMapper.getAllUsers();
+	public List<UserDto> getAllUsers(Map<String, Object> map) {
+		return adminMapper.getAllUsers(map);
+	}
+	
+	public List<PostDto> getAllPosts(Map<String, Object> map) {
+		return adminMapper.getAllPosts(map);
+	}
+	
+	public List<CommentDto> getAllComments(Map<String, Object> map) {
+		return adminMapper.getAllComments(map);
+	}
+	
+	public List<ReviewDto> getallReviews(Map<String, Object> map) {
+		return adminMapper.getAllReviews(map);
+	}
+	
+	public List<InquiryDto> getAllInquiries(Map<String, Object> map) {
+		return adminMapper.getAllInquiries(map);
+	}
+	
+	public List<InquiryDto> getHandledInquiries(Map<String, Object> map) {
+		return adminMapper.getHandledInquiries(map);
+	}
+	
+	public List<InquiryDto> getUnhandledInquiries(Map<String, Object> map) {
+		return adminMapper.getUnhandledInquiries(map);
 	}
 }
